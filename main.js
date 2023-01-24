c07f2d0b2fd8b419eba727602c984bb637131f3c
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-24 17:12:32 -0500

    Functioning Handlebars

--- main.js
+++ main.js
@@ -5,39 +5,24 @@
 
   console.log(data);
 
-  console.log(data.results[0]);
+  console.log(data.results);
 
   /* The source is used as the template. Outline the setup of the etsy picture, info, etc. */
   const source = document.getElementById("etsy-item-template").innerHTML;
 
-  /* Can reference a compile method on the Handlebars object. This const will be a function 
+  /* Can reference a compile method on the Handlebars object. This const will be a function */
   const template = Handlebars.compile(source);
 
-  /* This is a javascript object 
+  /* This is a javascript object */
 
-  I already have these values. How do I poke into the title?
-
-
-
-const etsyListing = {
-  results: [{title: data.results.0.description, url: }]
-}
-  
-  
-  const context = {
-    results: [
-      { title: "My New Post", body: "this is my first post" },
-      { title: "My Second Post", body: "this is my second post" },
-      { title: "My Third Post", body: "this is my third post" },
-    ],
-  };
+  const context = data;
 
   const html = template(context);
 
   console.log(html);
-  /* Now target and insert that html. Injecting it into your browser. 
+  /* Now target and insert that html. Injecting it into your browser. */
 
-  document.getElementById("news").insertAdjacentHTML("afterbegin", html);
-
-  */
+  document
+    .getElementById("loadCardsHere")
+    .insertAdjacentHTML("afterbegin", html);
 })();
